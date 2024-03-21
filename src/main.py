aed6fcd0d1b32cb28ef0c9dbfce4724059508d4d
lexor88 <maksimleksin88@gmail.com>
2024-03-21 20:47:35 +0200

    write test and edit path work

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import os.path
 from src.utils import load_json, get_filtered_list, get_sorted_list, print_message
+from config import ROOT_DIR
+
+PATH_TO_OPERATIONS = os.path.join(ROOT_DIR, "src", "operations.json")
 
 
 def main():
-    response=load_json("operations.json")
+    response=load_json(PATH_TO_OPERATIONS)
     filtered_list=get_filtered_list(response)
     sorted_list=get_sorted_list(filtered_list)
     for operation in sorted_list[:5]:
